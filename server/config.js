889848d878cc5d1c80ecf0dfea31583f8960c62d
Henrik <Henriko99@live.no>
2024-09-30 16:05:10 +0200

    Add ESlint and prettier to /server

--- server/config.js
+++ server/config.js
@@ -1,7 +1,7 @@
 // Export function so dotenv.config will be applied first.
 export function getConfig() {
-    return {
-      port: process.env.PORT || 3001,
-      mongoDBURL: process.env.MONGODB_URL,
-    };
+  return {
+    port: process.env.PORT || 3001,
+    mongoDBURL: process.env.MONGODB_URL,
+  };
 }
--- server/server.js
+++ server/server.js
@@ -1,22 +1,21 @@
 // Import and configure env variables
-import dotenv from 'dotenv';
+import dotenv from "dotenv";
 dotenv.config();
 
-import { getConfig } from './config.js';
+import { getConfig } from "./config.js";
 const { port, mongoDBURL } = getConfig();
 
-import express, { request, response } from 'express';
-import mongoose, { mongo } from 'mongoose';
-import cors from 'cors';
-import http from 'http';
-import { Server } from 'socket.io';
+import express from "express";
+import mongoose from "mongoose";
+import cors from "cors";
+import http from "http";
+import { Server } from "socket.io";
 
 const app = express();
 const server = http.createServer(app);
-
 const io = new Server(server, {
   cors: {
-    origin: '*', //TODO: Change for prod
+    origin: "*", //TODO: Change for prod
   },
 });
 
@@ -37,8 +36,8 @@ const canvasStates = {
   room1: null,
   room2: null,
   room3: null,
-  room4: null
-}
+  room4: null,
+};
 
 let users = [];
 const messages = {
@@ -46,104 +45,105 @@ const messages = {
   room1: [],
   room2: [],
   room3: [],
-  room4: []
+  room4: [],
 };
 
-io.on('connection', (socket) => {
-  console.log('connection');
-  
-  socket.on('join-server', (username) => {
+io.on("connection", (socket) => {
+  console.log("connection");
+
+  socket.on("join-server", (username) => {
     const user = {
-      username, 
+      username,
       id: socket.id,
     };
     users.push(user);
-    io.emit('new-user', users);
+    io.emit("new-user", users);
   });
 
   // Remove user from users when disconnecting
-  socket.on('disconnect', () => {
-    users = users.filter(u => u.id !== socket.id);
-    io.emit('new-user', users)
-  })
-
-
-  socket.on('join-room', (roomName, previousRoom) => {
-    if (previousRoom) {
-      socket.leave(previousRoom)
-    }
-    socket.join(roomName);
-    socket.emit('joined-room', { messages: messages[roomName], room: roomName, previousRoom: previousRoom });
-    
+  socket.on("disconnect", () => {
+    users = users.filter((u) => u.id !== socket.id);
+    io.emit("new-user", users);
   });
 
-  socket.on('send-message', ({content, sender, roomName}) => {
+  socket.on("join-room", (roomName, previousRoom) => {
+    if (previousRoom) {
+      socket.leave(previousRoom);
+    }
+    socket.join(roomName);
+    socket.emit("joined-room", {
+      messages: messages[roomName],
+      room: roomName,
+      previousRoom: previousRoom,
+    });
+  });
+
+  socket.on("send-message", ({ content, sender, roomName }) => {
     const payload = {
-      content, 
+      content,
       sender,
       roomName,
     };
-    socket.to(roomName).emit('new-message', payload)
-    
+    socket.to(roomName).emit("new-message", payload);
+
     if (messages[roomName]) {
       messages[roomName].push({
         sender,
-        content
+        content,
       });
     }
-  })
+  });
 
   // Get canvas state from a client, for a new client
-  socket.on('client-ready', async (room) => {
+  socket.on("client-ready", async (room) => {
     const users_in_room = (await io.in(room).fetchSockets()).length;
     if (users_in_room > 1) {
-      console.log('fetching canvasState from clients')
-      socket.to(room).emit('get-canvas-state');
+      console.log("fetching canvasState from clients");
+      socket.to(room).emit("get-canvas-state");
     }
     if (canvasStates[room]) {
-      console.log('canvasState exists, udating from server storage:', room)
-      socket.emit('canvas-state-from-server', canvasStates[room]);
+      console.log("canvasState exists, udating from server storage:", room);
+      socket.emit("canvas-state-from-server", canvasStates[room]);
     } else {
-      console.log('no canvas')
+      console.log("no canvas");
     }
-    
   });
 
-  socket.on('save-canvas', ({ room, state}) => {
+  socket.on("save-canvas", ({ room, state }) => {
     canvasStates[room] = state;
-    console.log('saved canvas to room', room)
-  })
-  socket.on('canvas-state', ({ room, state }) => {
+    console.log("saved canvas to room", room);
+  });
+  socket.on("canvas-state", ({ room, state }) => {
     canvasStates[room] = state;
-    console.log('saved canvas to room', room)
-    socket.to(room).emit('canvas-state-from-server', state);
+    console.log("saved canvas to room", room);
+    socket.to(room).emit("canvas-state-from-server", state);
   });
 
-  socket.on('draw-line', ({ room, prevPoint, currentPoint, color }) => {
-    socket.to(room).emit('draw-line', { prevPoint, currentPoint, color });
+  socket.on("draw-line", ({ room, prevPoint, currentPoint, color }) => {
+    socket.to(room).emit("draw-line", { prevPoint, currentPoint, color });
   });
 
-  socket.on('clear', (room) => {
+  socket.on("clear", (room) => {
     canvasStates[room] = null;
-    console.log('Emitting clear for room:', { recievedRoom: room });
-    io.to(room).emit('clear', room);
+    console.log("Emitting clear for room:", { recievedRoom: room });
+    io.to(room).emit("clear", room);
   });
 });
 
 // Routes
-app.get('/api', (req, res) => {
-  res.json({ users: ['userOne', 'userTwo', 'userThree'] });
+app.get("/api", (req, res) => {
+  res.json({ users: ["userOne", "userTwo", "userThree"] });
 });
 
-app.get('/', (req, res) => {
-  return res.status(234).send('Welcome!');
+app.get("/", (req, res) => {
+  return res.status(234).send("Welcome!");
 });
 
 // Start the server
 mongoose
   .connect(mongoDBURL)
   .then(() => {
-    console.log('App connected to database');
+    console.log("App connected to database");
     server.listen(port, () => {
       console.log(`Server is running on http://localhost:${port}`);
     });
